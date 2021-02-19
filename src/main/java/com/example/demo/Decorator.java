5678c3f2ec7341ce0320570599e89c96d83787f1
Archimedes Trajano <archimedes.trajano@devhaus.com>
2021-02-19 13:24:27 -0500

    Attempted to add Ordered no luck

--- src/main/java/com/example/demo/Decorator.java
+++ src/main/java/com/example/demo/Decorator.java
@@ -4,14 +4,22 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 
 @Configuration
-public class Decorator implements BeanPostProcessor, InitializingBean {
+public class Decorator implements BeanPostProcessor, InitializingBean, Ordered {
     @Value("${spring.kafka.maximumRequestSize:15728640}")
     private int maxRequestSize;
 
     @Override
     public void afterPropertiesSet() {
         System.out.println(maxRequestSize);
+    }
+
+    @Override
+    public int getOrder() {
+        // return HIGHEST_PRECEDENCE;
+        // return LOWEST_PRECEDENCE;
+        return 0;
     }
 }
